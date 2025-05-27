392c320b77c9a326bd5c3d93900d0162cd1a10c6
Emilio Quintero Fernández <56022080+Emiquifer@users.noreply.github.com>
2025-05-28 01:08:00 +0200

    Recursos 03

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float inputH;
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float fuerzaSalto;
+    private Animator anim;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimiento();
+        Saltar();
+        Atacar();
+    }
+
+    private void Movimiento()
+    {
+        inputH = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(inputH * velocidadMovimiento, rb.linearVelocity.y);
+
+        if (inputH != 0)
+        {
+            anim.SetBool("running", true);
+            if(inputH > 0)
+            {
+                transform.eulerAngles = Vector3.zero;
+            } else
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+    }
+
+    private void Saltar()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+            anim.SetTrigger("jump");
+        }
+    }
+
+    private void Atacar()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("attack");
+        }
+    }
+}
